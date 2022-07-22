6450bc31daed4458b53983f5b29673dd94ee22b3
guillaume.dorce <guillaume.dorce@bm-services.com>
2022-07-22 10:34:39 +0200

    change database name variable

--- db.js
+++ db.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-const dbUrl = process.env.DB_URL.replace("<username>:<password>", `${username}:${password}`);
+const database = process.env.DB_DATABASE;
+const dbUrl = process.env.DB_URL.replace("<username>:<password>", `${username}:${password}`).replace(
+  "<database>",
+  database
+);
 
-mongoose.connect(dbUrl)
-    .then(() => console.log("connected to mongodb"))
-    .catch(err => console.error(err));
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("connected to mongodb"))
+  .catch(err => console.error(err));
 
 const userSchema = new mongoose.Schema({
-    email: { type: String },
-    password: { type: String },
+  email: { type: String },
+  password: { type: String }
 });
 const sauceSchema = new mongoose.Schema({
-    userId: String,
-    name: String,
-    manufacturer: String,
-    description: String,
-    mainPepper: String,
-    imageUrl: String,
-    heat: Number,
-    likes: Number,
-    dislikes: Number,
-    usersLiked: [{ userId: String }],
-    usersDisliked: [{ userId: String }]
-})
+  userId: String,
+  name: String,
+  manufacturer: String,
+  description: String,
+  mainPepper: String,
+  imageUrl: String,
+  heat: Number,
+  likes: Number,
+  dislikes: Number,
+  usersLiked: [{ userId: String }],
+  usersDisliked: [{ userId: String }]
+});
 
 let User = mongoose.model("User", userSchema);
 let Sauce = mongoose.model("Sauce", sauceSchema);
